a6aef3073117377b8b5cbd243ef40b04e927296d
Ethan Peacock <Peacockethan@gmail.com>
2017-06-12 05:00:11 -0500

    Iterate through each Chessboard Image and draw corners
    added directory where images with corners drawn are added and code to iterate through each image , add to arrays, draw corners, and save images

--- lane_finding.py
+++ lane_finding.py
@@ -5,10 +5,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import glob
 %matplotlib inline
 
 # In[1]
 # Compute the camera calibration matrix and distortion coefficents
+
+# Read in and make a list of calibration images
+images = glob. glob('camera_cal/calibration*.jpg')
 
 # Read in a calibration image
 img = mpimg.imread('camera_cal/calibration2.jpg')
@@ -26,17 +30,23 @@ imgpoints = [] # 2D points in image plane
 objp = np.zeros((6*9,3), np.float32)
 objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2) # x, y coordinates
 
-# Convert image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Iterate through each image file, detecting corenrs, and appending points to the obj and img Arrays
+for fname in images:
+    # Read in each image
+    img = mpimg.imread(fname)
 
-# Find the chessboard corners
-ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-if ret == True:
-    imgpoints.append(corners)
-    objpoints.append(objp)
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
 
-    #draw and display the corners
-    img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
-    plt.imshow(img)
-    plt.savefig('drawChessboardCorners.jpg')
+    # If corners are found, add object points, image points
+    if ret == True:
+        imgpoints.append(corners)
+        objpoints.append(objp)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        plt.imshow(img)
+        plt.savefig('draw_corners/' + str(fname))
