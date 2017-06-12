c08b4e1fb11517d4e84a93ffa52985ce50a6abaa
Ethan Peacock <Peacockethan@gmail.com>
2017-06-12 04:24:18 -0500

    Find Chessboard Corners
    Finding and drawing chessboard corners using open cv2

--- lane_finding.py
+++ lane_finding.py
@@ -11,7 +11,7 @@ import matplotlib.image as mpimg
 # Compute the camera calibration matrix and distortion coefficents
 
 # Read in a calibration image
-img = mpimg.imread('camera_cal/calibration1.jpg')
+img = mpimg.imread('camera_cal/calibration2.jpg')
 plt.imshow(img)
 
 
@@ -20,3 +20,23 @@ plt.imshow(img)
 
 objpoints = [] # #D points in real world space
 imgpoints = [] # 2D points in image plane
+
+
+# Prepare object points, like (0,0,0), (1,0,0), (2,0,0), ....., (7,5,0) for points on chessboard
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2) # x, y coordinates
+
+# Convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Find the chessboard corners
+ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+if ret == True:
+    imgpoints.append(corners)
+    objpoints.append(objp)
+
+    #draw and display the corners
+    img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+    plt.imshow(img)
+    plt.savefig('drawChessboardCorners.jpg')
