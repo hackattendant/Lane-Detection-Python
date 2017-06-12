f42758754f024e3e61ca7bbdc02b5da7e8548b9c
Ethan Peacock <Peacockethan@gmail.com>
2017-06-12 05:27:21 -0500

    Undistorted chessboard image
    code undistorts chessboard and saves comparison photo into output_images

--- lane_finding.py
+++ lane_finding.py
@@ -51,6 +51,8 @@ for fname in images:
         plt.imshow(img)
         plt.savefig('output_images/draw_corners/' + str(fname))
         plt.savefig('output_images/cornersDrawnOn.jpg')
+
+
 # In[2]
 # Undistort image
 def cal_undistort(img, objpoints, imgpoints):
@@ -58,4 +60,16 @@ def cal_undistort(img, objpoints, imgpoints):
     undist = cv2.undistort(img, mtx, dist, None, mtx)
     return undist
 
-undistorted = cal_undistort(img, objpoints, imgpoints)
+distorted_img = mpimg.imread('camera_cal/calibration1.jpg')
+
+undistorted = cal_undistort(distorted_img, objpoints, imgpoints)
+
+# Visualize results of undistortion
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(distorted_img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(undistorted)
+ax2.set_title('Undistorted Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.savefig('output_images/undistortedImage.jpg')
