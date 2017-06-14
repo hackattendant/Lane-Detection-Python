2be41ccacbb21032780040ded4e338711d2b8a2c
Ethan Peacock <Peacockethan@gmail.com>
2017-06-14 18:13:49 -0500

    put blocks 13 - 15 in a different file to focus on code that is broken

---
+++ removed.py
@@ -0,0 +1,93 @@
+# In[13]
+"""
+# Measuring Curvature
+# Generate some fake data to represent lane-line pixels
+ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
+quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
+# For each y position generate random x position within +/-50 pix
+# of the line base position in each case (x=200 for left, and x=900 for right)
+leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51)
+                              for y in ploty])
+rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51)
+                                for y in ploty])
+
+leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+
+# Fit a second order polynomial to pixel positions in each fake lane line
+left_fit = np.polyfit(ploty, leftx, 2)
+left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+right_fit = np.polyfit(ploty, rightx, 2)
+right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+# Plot up the fake data
+mark_size = 3
+plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
+plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
+plt.xlim(0, 1280)
+plt.ylim(0, 720)
+plt.plot(left_fitx, ploty, color='green', linewidth=3)
+plt.plot(right_fitx, ploty, color='green', linewidth=3)
+plt.gca().invert_yaxis() # to visualize as we do the images
+
+
+# Calculate the radius of Curvature
+# Define y-value where we want radius of curvature
+# I'll choose the maximum y-value, corresponding to the bottom of the image
+y_eval = np.max(ploty)
+left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+print(left_curverad, right_curverad)
+
+
+
+
+
+
+# In[14]
+
+# calculate radius of curvature after correcting for scale in x and y
+# Define conversions in x and y from pixels space to meters
+ym_per_pix = 30/720 # meters per pixel in y dimension
+xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+# Fit new polynomials to x,y in world space
+left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+# Calculate the new radii of curvature
+left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+# Now our radius of curvature is in meters
+print(left_curverad, 'm', right_curverad, 'm')
+
+
+
+
+
+# In[15]
+# Define a class to receive the characteristics of each line detection
+class Line():
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False
+        # x values of the last n fits of the line
+        self.recent_xfitted = []
+        #average x values of the fitted line over the last n iterations
+        self.bestx = None
+        #polynomial coefficients averaged over the last n iterations
+        self.best_fit = None
+        #polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]
+        #radius of curvature of the line in some units
+        self.radius_of_curvature = None
+        #distance in meters of vehicle center from the line
+        self.line_base_pos = None
+        #difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float')
+        #x values for detected line pixels
+        self.allx = None
+        #y values for detected line pixels
+
+        self.ally = None
+"""
